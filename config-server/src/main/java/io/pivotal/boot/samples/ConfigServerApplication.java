6016d870449170e004414e1fb4d5f732e9fb4f01
Vinicius Carvalho <viniciusccarvalho@gmail.com>
2016-06-07 19:13:45 -0400

    Local cloud examples

--- config-server/src/main/java/io/pivotal/boot/samples/ConfigServerApplication.java
+++ config-server/src/main/java/io/pivotal/boot/samples/ConfigServerApplication.java
@@ -1,9 +1,15 @@
-package org.springframework.boot;
+package io.pivotal.boot.samples;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
+@Configuration
+@EnableAutoConfiguration
+@EnableConfigServer
 public class ConfigServerApplication {
 
 	public static void main(String[] args) {
--- details-service/src/main/java/io/pivotal/boot/samples/DetailsServiceApplication.java
+++ details-service/src/main/java/io/pivotal/boot/samples/DetailsServiceApplication.java
@@ -5,19 +5,19 @@ import io.pivotal.boot.samples.config.DetailProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableConfigurationProperties(DetailProperties.class)
+@EnableDiscoveryClient
 public class DetailsServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DetailsServiceApplication.class, args);
 	}
 
-	@Bean
-	public RestTemplate restTemplate(){
-		return  new RestTemplate();
-	}
+
 }
