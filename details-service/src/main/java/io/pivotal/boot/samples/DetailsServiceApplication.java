f2060e1919e76a533f58abbb98ed2d44251ba961
Vinicius Carvalho <viniciusccarvalho@gmail.com>
2016-06-07 20:06:17 -0400

    adding hystrix

--- details-service/src/main/java/io/pivotal/boot/samples/DetailsServiceApplication.java
+++ details-service/src/main/java/io/pivotal/boot/samples/DetailsServiceApplication.java
@@ -5,6 +5,7 @@ import io.pivotal.boot.samples.config.DetailProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
@@ -13,6 +14,7 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 @EnableConfigurationProperties(DetailProperties.class)
 @EnableDiscoveryClient
+@EnableCircuitBreaker
 public class DetailsServiceApplication {
 
 	public static void main(String[] args) {
